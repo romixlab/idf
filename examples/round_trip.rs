4fa36009e6487eec66cc456db234b67c1d1208a5
Roman Isaikin <romix.lab@gmail.com>
2023-10-20 13:43:40 +0200

    Add round trip example.

---
+++ examples/round_trip.rs
@@ -0,0 +1,15 @@
+use std::env;
+use either::Either;
+use idf::idf30::Idf30;
+
+fn main() {
+    let path = env::args().into_iter().skip(1).next().expect("IDF file path");
+    let contents = std::fs::read_to_string(path).unwrap();
+
+    let mut file = Idf30::parse(&contents).unwrap();
+    file.header.source = Either::Right(format!("rust_idf_{}", file.header.source));
+
+    println!("Name: {}\nComponents: {}", file.header.board_name, file.placement.len());
+
+    std::fs::write("./out.idf", file.to_string()).expect("Write file failed");
+}
